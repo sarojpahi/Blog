edd061ab3cd22888473817c027e293d412f05891
SarojPahi <107534043+SarojPahi@users.noreply.github.com>
2022-11-27 18:28:22 +0530

    postwidget created

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import moment from "moment";
+import Link from "next/link";
+import { getRecentPosts, getSimilarPosts } from "../services";
 
-export const PostWidget = () => {
-  return <div>PostWidget</div>;
+export const PostWidget = ({
+  categories,
+  slug,
+}: {
+  categories: any;
+  slug: any;
+}) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) => {
+        setRelatedPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setRelatedPosts(result);
+      });
+    }
+  }, [slug]);
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post: any, index: number) => (
+        <div key={index} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <Image
+              // loader={grpahCMSImageLoader}
+              alt={post.title}
+              height="50"
+              width="50"
+              unoptimized
+              priority
+              className="align-middle rounded-full h-auto w-auto"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("DD MMM, YYYY")}
+            </p>
+            <Link href={`/post/${post.slug}`} className="text-md" key={index}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
--- services/index.tsx
+++ services/index.tsx
@@ -1,5 +1,4 @@
 import { request, gql } from "graphql-request";
-import React from "react";
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT || "";
 export const getPosts = async () => {
   const query = gql`
@@ -34,4 +33,48 @@ export const getPosts = async () => {
 
   const result = await request(graphqlAPI, query);
   return result.postsConnection.edges;
+};
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async (categories: any, slug: any) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
 };
